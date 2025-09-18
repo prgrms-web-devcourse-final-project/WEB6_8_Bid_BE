9d805bc093412aca1fc534d99ae5ed9c8f2bf517
CYW815 <cho0815y@gmail.com>
2025-09-18 14:42:50 +0900

    Feat 회원가입 구현-1

--- src/main/java/com/backend/global/security/SecurityConfig.java
+++ src/main/java/com/backend/global/security/SecurityConfig.java
@@ -5,20 +5,29 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
+                        .requestMatchers("/api/v1/auth/signup").permitAll() // 회원가입 경로는 모두 허용
+                        .anyRequest().permitAll() // 현재는 다른 모든 요청도 허용 (추후 인증 필요 시 .authenticated()로 변경)
                 )
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .logout(AbstractHttpConfigurer::disable);
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
