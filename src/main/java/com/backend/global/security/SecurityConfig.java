f7a04899cfe4c89efdb1a2d3a2453911d175f166
eunchan96 <taegwon02@gmail.com>
2025-09-23 16:14:49 +0900

    [Remove]: import 제거

--- src/main/java/com/backend/global/security/SecurityConfig.java
+++ src/main/java/com/backend/global/security/SecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 
 @Configuration
 @RequiredArgsConstructor
